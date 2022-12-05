0b49ced14444f91424912c7a009afea91101ded9
Sravan Modala <45141111+sravanmodala1997@users.noreply.github.com>
2022-12-05 20:10:03 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,5 +1,5 @@
 class Program{
     public static void main(String[] args) {
-        System.out.println("Namaskaramu, welcome!!"); 
+        System.out.println("Namaskaramu, welcome to Git and Jenkins!!"); 
     }
 }
